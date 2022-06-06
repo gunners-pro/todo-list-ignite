271ac012442dbf09ff024272a13cebb8969bea6d
Fabricyo <bass.barreto@gmail.com>
2022-06-05 23:04:00 -0300

    It was styled button and input data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
+import { PlusCircle } from 'phosphor-react';
+import imgLogo from './assets/logo.svg';
+import styles from './app.module.css';
+
 function App() {
   return (
-    <h1>ABC</h1>
+    <div className={styles.page}>
+      <header>
+        <img src={imgLogo} alt="Logotipo" />
+      </header>
+      <main>
+        <div className={styles.inputData}>
+          <input
+            type="text"
+            placeholder="Adicione uma nova tarefa"
+          />
+          <button type="button">
+            Criar
+            <PlusCircle size={18} />
+          </button>
+        </div>
+      </main>
+    </div>
   )
 }
 
